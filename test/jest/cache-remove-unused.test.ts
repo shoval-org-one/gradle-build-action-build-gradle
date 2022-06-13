ee8c013b5bed926dfee617c72805ec8d07623655
Daz DeBoer <daz@gradle.com>
2022-06-13 11:58:48 -0600

    Extract CacheCleaner out of test

--- test/jest/cache-remove-unused.test.ts
+++ test/jest/cache-remove-unused.test.ts
@@ -1,27 +1,31 @@
 import * as exec from '@actions/exec'
 import fs from 'fs'
 import path from 'path'
+import {CacheCleaner} from '../../src/cache-cleaner'
 
 jest.setTimeout(60000)
 
 test('will cleanup unused dependency jars and build-cache entries', async () => {
     const projectRoot = prepareTestProject()
+    const gradleUserHome = path.resolve(projectRoot, 'HOME')
+    const tmpDir = path.resolve(projectRoot, 'tmp')
+    const cacheCleaner = new CacheCleaner(gradleUserHome, tmpDir)
 
     await runGradleBuild(projectRoot, 'build', '3.1')
     
-    await resetUsageState(projectRoot)
+    await cacheCleaner.prepare()
 
     await runGradleBuild(projectRoot, 'build', '3.1.1')
 
-    const commonsMath31 = path.resolve(projectRoot, "HOME/caches/modules-2/files-2.1/org.apache.commons/commons-math3/3.1")
-    const commonsMath311 = path.resolve(projectRoot, "HOME/caches/modules-2/files-2.1/org.apache.commons/commons-math3/3.1.1")
-    const buildCacheDir = path.resolve(projectRoot, "HOME/caches/build-cache-1")
+    const commonsMath31 = path.resolve(gradleUserHome, "caches/modules-2/files-2.1/org.apache.commons/commons-math3/3.1")
+    const commonsMath311 = path.resolve(gradleUserHome, "caches/modules-2/files-2.1/org.apache.commons/commons-math3/3.1.1")
+    const buildCacheDir = path.resolve(gradleUserHome, "caches/build-cache-1")
 
     expect(fs.existsSync(commonsMath31)).toBe(true)
     expect(fs.existsSync(commonsMath311)).toBe(true)
     expect(fs.readdirSync(buildCacheDir).length).toBe(4)
 
-    await forceUnusedCleanup(projectRoot)
+    await cacheCleaner.forceCleanup()
 
     expect(fs.existsSync(commonsMath31)).toBe(false)
     expect(fs.existsSync(commonsMath311)).toBe(true)
@@ -30,25 +34,28 @@ test('will cleanup unused dependency jars and build-cache entries', async () => 
 
 test('will cleanup unused gradle versions', async () => {
     const projectRoot = prepareTestProject()
+    const gradleUserHome = path.resolve(projectRoot, 'HOME')
+    const tmpDir = path.resolve(projectRoot, 'tmp')
+    const cacheCleaner = new CacheCleaner(gradleUserHome, tmpDir)
 
     // Initialize HOME with 2 different Gradle versions
     await runGradleWrapperBuild(projectRoot, 'build')
     await runGradleBuild(projectRoot, 'build')
     
-    await resetUsageState(projectRoot)
+    await cacheCleaner.prepare()
 
     // Run with only one of these versions
     await runGradleBuild(projectRoot, 'build')
 
-    const gradle733 = path.resolve(projectRoot, "HOME/caches/7.3.3")
-    const wrapper733 = path.resolve(projectRoot, "HOME/wrapper/dists/gradle-7.3.3-bin")
-    const gradle741 = path.resolve(projectRoot, "HOME/caches/7.4.1")
+    const gradle733 = path.resolve(gradleUserHome, "caches/7.3.3")
+    const wrapper733 = path.resolve(gradleUserHome, "wrapper/dists/gradle-7.3.3-bin")
+    const gradle741 = path.resolve(gradleUserHome, "caches/7.4.1")
 
     expect(fs.existsSync(gradle733)).toBe(true)
     expect(fs.existsSync(wrapper733)).toBe(true)
     expect(fs.existsSync(gradle741)).toBe(true)
 
-    await forceUnusedCleanup(projectRoot)
+    await cacheCleaner.forceCleanup()
 
     expect(fs.existsSync(gradle733)).toBe(false)
     expect(fs.existsSync(wrapper733)).toBe(false)
@@ -75,29 +82,4 @@ function prepareTestProject(): string {
     fs.rmSync(path.resolve(projectRoot, 'build'), { recursive: true, force: true })
     fs.rmSync(path.resolve(projectRoot, '.gradle'), { recursive: true, force: true })
     return projectRoot
-}
-
-async function resetUsageState(projectRoot: string): Promise<void> {
-    fs.rmSync(path.resolve(projectRoot, 'HOME/caches/journal-1'), { recursive: true, force: true })
-    fs.mkdirSync(path.resolve(projectRoot, 'HOME/caches/journal-1'), { recursive: true })
-    fs.writeFileSync(path.resolve(projectRoot, 'HOME/caches/journal-1/file-access.properties'), 'inceptionTimestamp=0')
-    await ageAllFiles(path.resolve(projectRoot, 'HOME'))
-    await touchAllFiles(path.resolve(projectRoot, 'HOME'), 'gc.properties')
-}
-
-async function forceUnusedCleanup(projectRoot: string) {
-    await ageAllFiles(path.resolve(projectRoot, 'HOME'), 'gc.properties')
-
-    const statusCleanup = await exec.exec('gradle -g HOME --no-daemon --build-cache help', [],{
-        cwd: projectRoot
-    })
-    console.log('Cleaned up HOME')
-}
-
-async function ageAllFiles(dirPath: string, fileName: string = '*'): Promise<void> {
-    await exec.exec('find', [dirPath, '-name', fileName, '-exec', 'touch', '-m', '-d', '1970-01-01', '{}', '+'], {})
-}
-
-async function touchAllFiles(dirPath: string, fileName: string = '*'): Promise<void> {
-    await exec.exec('find', [dirPath, '-name', fileName, '-exec', 'touch', '-m', '{}', '+'], {})
 }
