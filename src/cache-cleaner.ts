a88436a737e386b5bb525e819b0a8d698ada02e9
Daz DeBoer <daz@gradle.com>
2022-06-14 07:41:14 -0600

    Much more logging

--- src/cache-cleaner.ts
+++ src/cache-cleaner.ts
@@ -1,7 +1,9 @@
 import * as exec from '@actions/exec'
+import * as artifact from '@actions/artifact'
+import * as glob from '@actions/glob'
 import fs from 'fs'
 import path from 'path'
-import {cacheDebug} from './cache-utils'
+import {cacheDebug, isCacheDebuggingEnabled} from './cache-utils'
 
 export class CacheCleaner {
     private readonly gradleUserHome: string
@@ -14,6 +16,8 @@ export class CacheCleaner {
 
     async prepare(): Promise<void> {
         cacheDebug(`Preparing Gradle User Home for future cleanup`)
+
+        this.debugReportGradleUserHomeContents()
 
         fs.rmSync(path.resolve(this.gradleUserHome, 'caches/journal-1'), {recursive: true, force: true})
         fs.mkdirSync(path.resolve(this.gradleUserHome, 'caches/journal-1'), {recursive: true})
@@ -28,6 +32,9 @@ export class CacheCleaner {
     async forceCleanup(): Promise<void> {
         cacheDebug(`Forcing Gradle User Home cleanup`)
 
+        cacheDebug('BEFORE CLEANUP')
+        this.debugReportGradleUserHomeContents()
+
         await this.ageAllFiles('gc.properties')
 
         const cleanupProjectDir = path.resolve(this.tmpDir, 'dummy-cleanup-project')
@@ -41,6 +48,11 @@ export class CacheCleaner {
         await exec.exec(`gradle -g ${this.gradleUserHome} --no-daemon --build-cache --no-scan --quiet noop`, [], {
             cwd: cleanupProjectDir
         })
+
+        cacheDebug(`AFTER CLEANUP`)
+        this.debugReportGradleUserHomeContents()
+
+        this.uploadGradleUserHome()
     }
 
     private async ageAllFiles(fileName = '*'): Promise<void> {
@@ -53,5 +65,32 @@ export class CacheCleaner {
 
     private async touchAllFiles(fileName = '*'): Promise<void> {
         await exec.exec('find', [this.gradleUserHome, '-name', fileName, '-exec', 'touch', '-m', '{}', '+'], {})
+    }
+
+    private async debugReportGradleUserHomeContents(): Promise<void> {
+        if (!isCacheDebuggingEnabled()) {
+            return
+        }
+        if (!fs.existsSync(this.gradleUserHome)) {
+            return
+        }
+        await exec.exec('du', ['-a'], {
+            cwd: this.gradleUserHome,
+            silent: true,
+            ignoreReturnCode: true
+        })
+    }
+
+    private async uploadGradleUserHome(): Promise<void> {
+        if (!isCacheDebuggingEnabled()) {
+            return
+        }
+        if (!fs.existsSync(this.gradleUserHome)) {
+            return
+        }
+        const globber = await glob.create(`${this.gradleUserHome}/**/*`)
+        const rawSearchResults: string[] = await globber.glob()
+        const artifactClient = artifact.create()
+        await artifactClient.uploadArtifact('gradle-user-home', rawSearchResults, this.gradleUserHome)
     }
 }
